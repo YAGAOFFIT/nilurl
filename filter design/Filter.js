dc51a8c5c7aaaa5611d2dad5ebc769589ed0e5c0
Alexon <s_visochenko@mail.ru>
2023-09-01 14:28:51 +0700

    Добавление выпадающего списка

--- filter design/Filter.js
+++ filter design/Filter.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 
 
-function Filter() {
-  return (
-<div className="search">
 
+function Filters() {
+	const [isOpen, setOpen] = useState(false);
+	const [isOpenTag, setOpenTag] = useState(false);
+	return (
+		<div className="search">
 			<div className="search-iner">
 				<div className="search-iner__link">
 					<div className="search-iner__link_name">
@@ -16,41 +19,57 @@ function Filter() {
 								<line x1="21" x2="16.65" y1="21" y2="16.65"></line>
 							</svg>
 						</div>
-						<input type="text" className="search-filter__inp" placeholder="Поиск..." value=""/>
+						<input type="text" className="search-filter__inp" placeholder="Поиск..." value="" />
 					</div>
 				</div>
-	
 				<fieldset className="search-tag" id="frag">
 					<div className="search-tag__name" >
-						<button className="search-tag__btn" id="tag_name">
-							<svg id="icon" className="null" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
+						<button className="search-tag__btn" id="tag_name" onClick={() => setOpen(!isOpen)}>
+							<svg id="icon" className={`null   ${isOpen ? "rotate90" : ""} ${isOpenTag ? "rotate180" : ""}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
 								<polyline points="9 18 15 12 9 6"></polyline>
 							</svg>
-							<h4 id="name">Теги </h4>
+							<input id="inp-link" placeholder="Название" style={{ display: `${isOpenTag ? "grid" : "none"}` }} />
+							<h4 id="name" style={{ display: `${isOpenTag ? "none" : "grid"}` }}>Теги</h4>
 						</button>
-						<button className="search-tag__btn_add">Добавить</button>
+						<button className="search-tag__btn_add" onClick={() => setOpenTag(!isOpenTag)}>Добавить</button>
 					</div>
-					<div className="search-tag__list">
-	
+					{/* таблица ссылок */}
+					<div className="search-tag__list" style={{ display: `${isOpen ? "grid" : "none"}` }} >
 						<button className="search-tag__list_item">
-							<input id="dub.sh" name="dub.sh" type="radio" className="search-tag__list_item_inp-tag" checked=""/>
+							<input id="dub.sh" name="dub.sh" type="radio" className="search-tag__list_item_inp-tag" checked="" />
 							<label for="dub.sh" className="search-tag__list_item_name">
 								<p>dub.sh</p>
 							</label>
 						</button>
 						<button className="search-tag__list_item">
-							<input id="dub.sh" name="dub.sh" type="radio" className="search-tag__list_item_inp-tag" checked=""/>
+							<input id="dub.sh" name="dub.sh" type="radio" className="search-tag__list_item_inp-tag" checked="" />
 							<label for="dub.sh" className="search-tag__list_item_name">
 								<p>dub.sh</p>
 							</label>
 						</button>
-						
+					</div>
+					{/* таблица тегов */}
+					<div id="tag__list" style={{ display: `${isOpenTag ? "grid" : "none"}` }}>
+						<button id="0" className="link_item">
+							<input className="search-tag__list_item_inp" type="radio" />
+							<label className="search-tag__list_item_name">https://itchief.ru/javascript/create-and-append-elements</label>
+						</button>
+						<button id="1" className="link_item">
+							<input className="search-tag__list_item_inp" type="radio" />
+							<label className="search-tag__list_item_name">https://itchief.ru/javascript/create-and-append-elements</label>
+						</button><button id="2" className="link_item">
+							<input className="search-tag__list_item_inp" type="radio" />
+							<label className="search-tag__list_item_name">https://itchief.ru/javascript/create-and-append-elements</label>
+						</button>
+						<button id="3" className="link_item">
+							<input className="search-tag__list_item_inp" type="radio" />
+							<label className="search-tag__list_item_name">https://itchief.ru/javascript/create-and-append-elements</label>
+						</button>
 					</div>
 				</fieldset>
 			</div>
-	</div>
-
+		</div>
 	);
 }
 
-export default Filter;
+export default Filters;
