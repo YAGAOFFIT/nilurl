94a89b5f6e7b4fab1fd68e68107ae90984eaba33
Alexon <s_vysochenko@mail.ru>
2023-10-20 15:41:09 +0700

    jogin/sing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import Cab from './Cabinet';
 import "./Cabinet.css";
 import Reg from './regest/Regest';
-import Reges from './regest/Regest';
+import Log from './regest/Login';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Cab/> */}
-    <Reges/>
+    {/* <Log/> */}
+    <Log/>
   </React.StrictMode>
 );
 
--- src/regest/Regest.js
+++ src/regest/Regest.js
@@ -1,52 +1,25 @@
-import "./Regest.css";
+import "./Regest.css"
 
-function Reges() {
+function Reg() {
     return (
         <div className="d1">
-            <div className="d2_1">
-            <div class="slider-thumb"></div>
-                {/* <div className="d2_11"></div>
-                <div className="d2_12"></div>
-                <div className="d2_13"></div> */}
+            <div className="d2_1" style={{background: "linear-gradient(225deg, #e25186, #6059ff)"}}>
+                <div class="slider-thumb"></div>
             </div>
             <div className="d2_2">
                 <div className="d3_1">
                     <a href="https://nil-agency.ru" className="a3_1">
-                        <image />
                         <img src="/img/reg/nil logo icon.svg" width={40} />
-                        {/* <svg width="191" height="191" viewBox="0 0 191 191" fill="none" xmlns="http://www.w3.org/2000/svg" className="sv3">
-
-                            <g clip-path="url(#clip0_1301_107)">
-
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M122 -14H144 9.17859 129.788 5.96937 122 3.72462V-14ZM122 3.72462C113.586 1.29941 104.695 0 95.5 0C42.7568 0 0 42.7568 0 95.5C0 148.243 42.7568 191 95.5 191C148.243 191 191 148.243 191 95.5C191 60.462 172.131 29.8311 144 13.2146V100V148H122V140.897C114.258 146.018 104.977 149 95 149C67.938 149 46 127.062 46 100C46 72.938 67.938 51 95 51C104.977 51 114.258 53.982 122 59.1034V3.72462Z" fill="currentColor" shape-rendering="geometricPrecision">
-                                </path>
-                            </g>
-                            <defs>
-                                <clipPath id="clip0_1301_107">
-                                    <rect width="191" height="191" rx="95.5" fill="white">
-                                    </rect>
-                                </clipPath>
-                            </defs>
-                        </svg> */}
                     </a>
                     <h3 className="h3">Регистрация в NILUrl</h3>
                     <p className="p1">Начните создавать короткие ссылки.</p>
                 </div>
                 <div className="d3_2">
-                    
                     <form className="f3_1">
-
-                    <input type="text" data-t="field:input-login" dir="ltr" aria-invalid="false" autocorrect="off" autocapitalize="off" autocomplete="username" class="in3_1" id="passp-field-login" name="login" placeholder="Имя пользователя" value=""/>
-                        {/* <button type="button" className="b4">
-                            <p className="p4">Continue with Email</p>
-                        </button> */}
+                        <input type="text" data-t="field:input-login" dir="ltr" aria-invalid="false" autocorrect="off" autocapitalize="off" autocomplete="username" class="in3_1" id="passp-field-login" name="login" placeholder="Имя пользователя" value="" />
                     </form>
                     <form className="f3_2">
-                    <input type="text" data-t="field:input-login" dir="ltr" aria-invalid="false" autocorrect="off" autocapitalize="off" autocomplete="username" class="in3_1" id="passp-field-login" name="login" placeholder="email" value=""/>
-
-                        {/* <button type="button" className="b5">
-                            <p className="p5">Continue with SAML SSO</p>
-                        </button> */}
+                        <input type="text" data-t="field:input-login" dir="ltr" aria-invalid="false" autocorrect="off" autocapitalize="off" autocomplete="username" class="in3_1" id="passp-field-login" name="login" placeholder="email" value="" />
                     </form>
                     <button type="button" className="b3">
                         <p className="p2">Зарегистрироваться</p>
@@ -60,4 +33,4 @@ function Reges() {
     );
 }
 
-export default Reges
+export default Reg;
